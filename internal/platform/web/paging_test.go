2a91fc2a0fb01f6718d9fceb6cf12afe85ab6266
Jean-Marcel Belmont <jbelmont@foodlogiq.com>
2019-01-19 19:25:51 -0500

    Adding paging test for internal/platform/web package.

---
+++ internal/platform/web/paging_test.go
@@ -0,0 +1,25 @@
+package web_test
+
+import (
+	"net/url"
+	"testing"
+
+	"github.com/jbelmont/api-workshop/internal/platform/web"
+)
+
+func TestPopulatePaging(t *testing.T) {
+	url := url.URL{
+		Scheme:   "http",
+		Host:     "localhost:8080",
+		Path:     "/api/v1/heroes",
+		RawQuery: "pageSize=50&pageIndex=1",
+	}
+
+	paging := web.PopulatePaging(url, 50, 100)
+	if paging.Index != 1 {
+		t.Errorf("Expected %d, Actual %d", 1, paging.Index)
+	}
+	if paging.Size != 50 {
+		t.Errorf("Expected %d, Actual %d", 50, paging.Size)
+	}
+}
